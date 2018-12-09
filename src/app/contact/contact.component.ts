1a44734ff07482419d021e675bc06df64f1351fc
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-09 14:33:42 +0100

    Generates contact component

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.sass']
+})
+
+export class ContactComponent implements OnInit {
+  @Input() contact: any;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.contact = {
+      name: '',
+      email: '',
+      company: '',
+      role: '',
+      twitter: '',
+      location: '',
+      notes: ''
+    };
+  }
+}
